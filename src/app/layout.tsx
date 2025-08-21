6d7bfcd54ff7b18a0b5163d7a2a20fa1040f988f
Ayush Tripathi <tripathiayush944@gmail.com>
2025-08-21 17:31:27 +0000

    put this icon to fav icon at the top of web site to

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,14 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+      <head />
       <body className="font-body antialiased">
         {children}
         <Toaster />
